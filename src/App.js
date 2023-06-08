cd03190c16521f6197e7dea6bf8d31e911c9cd2c
furkancankaya <furkancankayaaa@gmail.com>
2023-06-08 09:41:12 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        console.log("deneme");
-      </header>
-    </div>
+    <Container>
+      
+    </Container>
   );
 }
 
